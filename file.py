59d9821ea4fd2d0a92bbbbd853205e5a45f6face
devkaranpatidar <devkaranpatidar@rediffmail.com>
2017-01-30 17:52:15 +0530

    freshh
    new

--- file.py
+++ file.py
@@ -1,10 +1,10 @@
 "Add" 
-<<<<<<< HEAD
+ HEAD
 tg435reg5h6th
-=======
+
 tg435reg5h6th
 php.py file update by dev 
 
 and time now 3.37 at now
 tg435reg5h6th  wefkwk ewrhoiwe ewrjhweoir ewehryoewifr
->>>>>>> origin/master
+
